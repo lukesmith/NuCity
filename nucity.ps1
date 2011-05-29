45abd6d2981ffecf3a2a01875b1bfb5ef19d8377
lukesmith <stuff@lukesmith.net>
2011-05-29 10:59:07 +0100

    Initial checkin

---
+++ nucity.ps1
@@ -0,0 +1,21 @@
+$nugetCore = resolve-path "..\packages\NuGet.Core*\lib\net40\NuGet.Core.dll"
+$Assembly = [Reflection.Assembly]::LoadFrom($nugetCore);
+
+$Uri = New-Object System.Uri("http://packages.nuget.org/v1/FeedService.svc/")
+$DataService = New-Object -TypeName "NuGet.DataServicePackageRepository"  -ArgumentList $Uri
+
+$localRepository = New-Object NuGet.LocalPackageRepository(resolve-path "..\packages")
+$localPackages = $localRepository.GetPackages()
+$outOfDatePackages = [NuGet.PackageRepositoryExtensions]::GetUpdates($DataService, $localPackages)
+
+$outputFile = "results\outofdatepackages.html"
+if (Test-Path $outputFile) { Clear-Content $outputFile }
+Add-Content $outputFile "<html><body>"
+Add-Content $outputFile "<h1>An update exists for the following packages</h1>"
+Add-Content $outputFile "<ul>"
+foreach ($packageUpdate in $outOfDatePackages) {
+	$item = "<li>" + $packageUpdate.Id + " -> " + $packageUpdate.Version + "</li>"
+	Add-Content $outputFile $item
+}
+Add-Content $outputFile "</ul>"
+Add-Content $outputFile "</body></html>"
